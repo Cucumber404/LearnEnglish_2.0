0f91519340f7bf4baaf9d006407ddf5638161d5b
Cucumber404 <ilya.zenchenko@icloud.com>
2022-05-08 15:31:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/learnenglish_20/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.learnenglish_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.learnenglish_20.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding; // В binding будут все элементы дизайна у которых есть id
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); //Создаем объект класса ResultProfileBinding – в binding все id
+        setContentView(binding.getRoot()); // Корневая id – Activity main
+
+        replaceFragment(new HomeFragment()); // Чтобы при открытии приложения показывался HomeFragment
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> { // Слушатель нажатия на  bottomNavigationView
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    public void replaceFragment(Fragment fragment){ // Замена фрагмента
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
